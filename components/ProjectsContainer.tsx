748b5694207a4d373913ed6e1de9e5f84095eaab
Chloe Court <crc.courtois@gmail.com>
2023-01-27 22:41:21 +0100

    removed comments

--- components/ProjectsContainer.tsx
+++ components/ProjectsContainer.tsx
@@ -6,8 +6,7 @@ export const ProjectsContainer = () => {
       <h2 className="bg-white rounded-lg shadow-xl text-center text-lg font-medium text-gray-600 px-5 py-3">
         Boost Excel: Add-in to use SQL to Query Data
       </h2>
-      <WindowMockUp src="https://boost-splash-page-5im6-movkazaht-chloecourt.vercel.app" />
-      {/* <h4 className="text-lg font-medium ">TECH STACK</h4> */}
+      <WindowMockUp src="https://boost-splash-page-chloecourt.vercel.app/" />
       <span className="mx-auto text-center block">TECH STACK</span>
       <p className="max-w-2xl bg-white/50 rounded-lg shadow-xl p-5 text-center">
         Microsoft Excel API, React, TypeScript, Node.js
